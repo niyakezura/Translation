d730ed00ac988d07577162f1e979386215a03f84
みそしる <sasaryo9125@gmail.com>
2024-08-21 15:46:42 +0900

    JavaScirptちょい修正

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const translateButton = document.getElementById('trans_button');
-    let text = document.getElementById('trans_text').value; // 翻訳するテキストを取得
-    let sourceLang = document.getElementById('sourceLang').value; // 現在の入力言語を取得
-    let targetLang = document.getElementById('targetLang').value; // 現在の出力言語を取得
-    const reverseButton = document.getElementsByClassName('reverseButton')[0]; // reverseLangボタンの取得
+    let text = document.getElementById('trans_text').value;
+    let sourceLang = document.getElementById('sourceLang').value;
+    let targetLang = document.getElementById('targetLang').value;
+    const reverseButton = document.getElementsByClassName('reverseButton')[0];
 
     translateButton.addEventListener('click', () => {
-        text = document.getElementById('trans_text').value; // 翻訳するテキストを取得
-        sourceLang = document.getElementById('sourceLang').value; // 現在の入力言語を取得
-        targetLang = document.getElementById('targetLang').value; // 現在の出力言語を取得
+        text = document.getElementById('trans_text').value;
+        sourceLang = document.getElementById('sourceLang').value;
+        targetLang = document.getElementById('targetLang').value;
 
         const API_KEY = '274449d3-cad7-4a56-b8b6-b8db3dfe510c:fx';
         const url = `https://api-free.deepl.com/v2/translate?auth_key=${API_KEY}&text=${encodeURIComponent(text)}&source_lang=${sourceLang}&target_lang=${targetLang}`;
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 return response.json();
             })
             .then(result => {
-                document.getElementById('trans_outtext').value = result.translations[0].text; // 出力テキストのIDを修正
+                document.getElementById('trans_outtext').value = result.translations[0].text;
             })
             .catch(error => {
                 console.error('Error:', error);
